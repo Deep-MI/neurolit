78221d1e61cf6a1a2437652e4227aeec359bdf58
Martin Reuter <reuter@mit.edu>
2026-02-10 12:29:31 +0100

    remove had-coded version in doc/conf.py and include read_the_docs theme

--- doc/conf.py
+++ doc/conf.py
@@ -12,11 +12,12 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'LIT - Lesion Inpainting Tool'
-copyright = '2025, Clemens Pollak, David Kuegler, Martin Reuter et al.'
+project = 'neuroLIT'
 author = 'Clemens Pollak, David Kuegler, Martin Reuter et al.'
-release = '0.5.1'
-version = '0.5.1'
+copyright = f"{date.today().year}, {author}"
+release = neurolit.__version__
+package = neurolit.__name__
+gh_url = "https://github.com/Deep-MI/neurolit"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
